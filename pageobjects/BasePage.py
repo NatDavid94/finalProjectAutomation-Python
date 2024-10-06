9a5e836deed877991d8e4b61986be7686c764540
NatDavid94 <ntndavidov7@gmail.com>
2024-10-06 12:14:11 +0300

    Your commit message here

--- pageobjects/BasePage.py
+++ pageobjects/BasePage.py
@@ -8,9 +8,11 @@ class BasePage:
         self.driver: WebDriver = driver
 
     def click(self, locator) -> None:
+        self.highlight_element(locator, "Yellow")
         self.driver.find_element(*locator).click()
 
     def fill_text(self, locator, txt: str) -> None:
+        self.highlight_element(locator, "Yellow")
         el = self.driver.find_element(*locator)
         el.clear()
         el.send_keys(txt)
@@ -19,3 +21,15 @@ class BasePage:
         return self.driver.find_element(*locator).text
 
 
+    # Function to highlight an element with a specified color
+    def highlight_element(self, locator, color):
+        element = self.driver.find_element(*locator)
+        original_style = element.get_attribute("style")
+        new_style = "background-color: " + color + ";" + original_style
+
+        # Change the style temporarily
+        self.driver.execute_script("arguments[0].setAttribute('style', arguments[1]);", element, new_style)
+
+        # Change the style back after a few milliseconds
+        self.driver.execute_script("setTimeout(function () { arguments[0].setAttribute('style', arguments[1]); }, 400);"
+                                   , element, original_style)
